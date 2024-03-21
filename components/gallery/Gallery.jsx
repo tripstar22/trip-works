e8b38d24b3ddc151aa35ae3262bccb5ef61d64e6
tripstar22 <trip.pruitt@gmail.com>
2024-03-21 10:01:41 -0400

    Modify Gallery component so that gallery items only show up after media has loaded

--- components/gallery/Gallery.jsx
+++ components/gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 // * react imports *
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // * third party library imports *
 import PropTypes from 'prop-types';
@@ -22,6 +22,7 @@ import classes from './gallery.module.scss';
 function Gallery({ media }) {
   // * state *
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
   const [open, setOpen] = useState(false);
 
   // * handlers *
@@ -43,6 +44,10 @@ function Gallery({ media }) {
     setOpen(false);
   };
 
+  useEffect(() => {
+    setLoading(false);
+  }, [media]);
+
   return (
     <section className={`section ${classes.gallery}`}>
       <div className={classes.gallery_container}>
@@ -58,63 +63,81 @@ function Gallery({ media }) {
                 Gallery
               </Typography>
             </Grid>
-            {media.map((item, index) => (
-              <Grid key={item.fields.file.fileName} item xs={12} sm={6} md={4}>
-                <AppLink
-                  href="#"
-                  aria-label={item.fields.title}
-                  className={classes.gallery_link}
-                  onClick={(event) => {
-                    event.preventDefault();
-                    toggleModalOpen();
-                    setCurrentSlide(index);
-                  }}
+            {loading ? (
+              <Typography className={classes.gallery_text} variant="body2">
+                Loading…
+              </Typography>
+            ) : (
+              media.map((item, index) => (
+                <Grid
+                  key={item.fields.file.fileName}
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
                 >
-                  <Card>
-                    <CardActionArea
-                      className={classes.gallery_action}
-                      component="div"
-                      tabIndex={-1}
-                    >
-                      {item.fields.file.contentType.startsWith('image/') ? (
-                        <CardMedia
-                          alt={item.fields.title}
-                          component="img"
-                          height="100%"
-                          src={item.fields.file.url}
-                        />
-                      ) : item.fields.file.contentType.startsWith('video/') ? (
-                        <div className={classes.gallery_item}>
+                  <AppLink
+                    href="#"
+                    aria-label={item.fields.title}
+                    className={classes.gallery_link}
+                    onClick={(event) => {
+                      event.preventDefault();
+                      toggleModalOpen();
+                      setCurrentSlide(index);
+                    }}
+                  >
+                    <Card>
+                      <CardActionArea
+                        className={classes.gallery_action}
+                        component="div"
+                        tabIndex={-1}
+                      >
+                        {item.fields.file.contentType.startsWith('image/') ? (
                           <CardMedia
                             alt={item.fields.title}
-                            component="video"
+                            component="img"
                             height="100%"
-                            preload="auto"
                             src={item.fields.file.url}
                           />
-                          <div className={classes.gallery_video}>
-                            <div className={classes.gallery_videoContainer}>
-                              <div className={classes.gallery_videoPlay}>
-                                <div
-                                  className={classes.gallery_videoPlayTriangle}
-                                />
+                        ) : item.fields.file.contentType.startsWith(
+                          'video/',
+                        ) ? (
+                          <div className={classes.gallery_item}>
+                            <CardMedia
+                              alt={item.fields.title}
+                              component="video"
+                              height="100%"
+                              preload="metadata"
+                              src={item.fields.file.url}
+                            />
+                            <div className={classes.gallery_video}>
+                              <div className={classes.gallery_videoContainer}>
+                                <div className={classes.gallery_videoPlay}>
+                                  <div
+                                    className={
+                                      classes.gallery_videoPlayTriangle
+                                    }
+                                  />
+                                </div>
+                                <Typography
+                                  className={classes.gallery_videoText}
+                                >
+                                  Watch Video
+                                </Typography>
                               </div>
-                              <Typography className={classes.gallery_videoText}>
-                                Watch Video
-                              </Typography>
                             </div>
                           </div>
-                        </div>
-                      ) : (
-                        <Typography variant="body1">
-                          Unsupported media type.
-                        </Typography>
-                      )}
-                    </CardActionArea>
-                  </Card>
-                </AppLink>
-              </Grid>
-            ))}
+                          ) : (
+                            <Typography variant="body1">
+                              Unsupported media type.
+                            </Typography>
+                          )}
+                      </CardActionArea>
+                    </Card>
+                  </AppLink>
+                </Grid>
+              ))
+            )}
           </Grid>
         </Container>
       </div>

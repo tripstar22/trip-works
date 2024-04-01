aa94120ea61349447d5b7997c0100ef8bdfa3539
tripstar22 <trip.pruitt@gmail.com>
2024-04-01 09:54:22 -0400

    Modify index.jsx event listener for document from DOMContentLoaded to readystatechange

--- pages/index.jsx
+++ pages/index.jsx
@@ -177,7 +177,7 @@ function IndexPage(props) {
     onDocReady();
 
     // * events *
-    document.addEventListener('DOMContentLoaded', onDocReady);
+    document.addEventListener('readystatechange', onDocReady);
 
     // * update state for isRendered once rendered *
     setIsRendered(true);
